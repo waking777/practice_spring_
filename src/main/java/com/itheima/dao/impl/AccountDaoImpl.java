10baf8d67b5afe5036ed54540adb4c69d84c1ccf
waking777 <756274132@qq.com>
2019-12-11 15:56:58 +0800

    one day fish and thirty days rest

--- src/main/java/com/itheima/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/itheima/dao/impl/AccountDaoImpl.java
@@ -1,5 +1,6 @@
 package com.itheima.dao.impl;
 
+import com.itheima.Utils.ConnectionUtils;
 import com.itheima.dao.IAccountDao;
 import com.itheima.domain.Account;
 import org.apache.commons.dbutils.QueryRunner;
@@ -14,14 +15,20 @@ import java.util.List;
 public class AccountDaoImpl implements IAccountDao {
 
     private QueryRunner runner;
+    private ConnectionUtils connectionUtils;
 
     public void setRunner(QueryRunner runner) {
         this.runner = runner;
     }
 
+    public void setConnectionUtils(ConnectionUtils connectionUtils) {
+        this.connectionUtils = connectionUtils;
+    }
+
     public List<Account> findAllAccount() {
         try {
-            return runner.query("select * from account", new BeanListHandler<Account>(Account.class));
+            return runner.query(connectionUtils.getThreadConnection(),"select * from account", new
+                    BeanListHandler<Account>(Account.class));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -29,7 +36,7 @@ public class AccountDaoImpl implements IAccountDao {
 
     public Account findAccountById(Integer accountId) {
         try {
-            return runner.query("select * from account where id = ?", new BeanHandler<Account>(Account.class),
+            return runner.query(connectionUtils.getThreadConnection(),"select * from account where id = ?", new BeanHandler<Account>(Account.class),
                     accountId);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -38,7 +45,7 @@ public class AccountDaoImpl implements IAccountDao {
 
     public void saveAccount(Account account) {
         try {
-            runner.update("insert into account(name,money) value(?,?)", account.getName(), account.getMoney());
+            runner.update(connectionUtils.getThreadConnection(),"insert into account(name,money) value(?,?)", account.getName(), account.getMoney());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -46,7 +53,7 @@ public class AccountDaoImpl implements IAccountDao {
 
     public void updateAccount(Account account) {
         try {
-            runner.update("update account set name=?,money=? where id = ?", account.getName(), account.getMoney(),
+            runner.update(connectionUtils.getThreadConnection(),"update account set name=?,money=? where id = ?", account.getName(), account.getMoney(),
                     account.getId());
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -55,7 +62,7 @@ public class AccountDaoImpl implements IAccountDao {
 
     public void deleteAccount(Integer accountId) {
         try {
-            runner.update("delete from account where id = ?", accountId);
+            runner.update(connectionUtils.getThreadConnection(),"delete from account where id = ?", accountId);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -63,7 +70,7 @@ public class AccountDaoImpl implements IAccountDao {
 
     public Account findAccountByName(String accountName) {
         try {
-            List<Account> accounts =  runner.query("select * from account where name = ?", new BeanListHandler<Account>
+            List<Account> accounts =  runner.query(connectionUtils.getThreadConnection(),"select * from account where name = ?", new BeanListHandler<Account>
                             (Account.class), accountName);
             if (accounts == null || accounts.size() == 0){
                 return null;
--- src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itheima.service.impl;
 
+import com.itheima.Utils.TransactionManager;
 import com.itheima.dao.IAccountDao;
 import com.itheima.domain.Account;
 import com.itheima.service.IAccountService;
@@ -8,19 +9,38 @@ import java.util.List;
 
 /**
  * 账户业务层的实现类
- *
- *
  */
 public class AccountServiceImpl implements IAccountService {
 
+    private TransactionManager txManager;
     private IAccountDao accountDao;
+
+    public void setTxManager(TransactionManager txManager) {
+        this.txManager = txManager;
+    }
 
     public void setAccountDao(IAccountDao accountDao) {
         this.accountDao = accountDao;
     }
 
     public List<Account> findAllAccount() {
-        return accountDao.findAllAccount();
+        try{
+            //1.开启事务
+            txManager.beginTransaction();
+            //2.执行操作
+            List<Account> accounts = accountDao.findAllAccount();
+            //3.提交事务
+            txManager.commit();
+            //4.返回结果
+            return accounts;
+        }catch(Exception e){
+            //5.回滚操作
+            txManager.rollback();
+            throw new RuntimeException(e);
+        }finally{
+            //6.释放资源
+            txManager.release();
+        }
     }
 
     public Account findAccountById(Integer accountId) {
@@ -40,17 +60,36 @@ public class AccountServiceImpl implements IAccountService {
     }
 
     public void transfer(String sourceName, String targetName, Float money) {
-        //1.根据名称查询转出账户
-        Account source = accountDao.findAccountByName(sourceName);
-        //2.根据名称查询转入账户
-        Account target = accountDao.findAccountByName(targetName);
-        //3.转出账户减钱
-        source.setMoney(source.getMoney()-money);
-        //4.转入账户加钱
-        target.setMoney(target.getMoney()+money);
-        //5.更新转出账户
-        updateAccount(source);
-        //6.更新转入账户
-        updateAccount(target);
+        try{
+            //1.开启事务
+            txManager.beginTransaction();
+            //2.执行操作
+
+            //1.根据名称查询转出账户
+            Account source = accountDao.findAccountByName(sourceName);
+            //2.根据名称查询转入账户
+            Account target = accountDao.findAccountByName(targetName);
+            //3.转出账户减钱
+            source.setMoney(source.getMoney() - money);
+            //4.转入账户加钱
+            target.setMoney(target.getMoney() + money);
+            //5.更新转出账户
+            updateAccount(source);
+
+//            int i = 1/0;
+
+            //6.更新转入账户
+            updateAccount(target);
+
+            //3.提交事务
+            txManager.commit();
+        }catch(Exception e){
+            //5.回滚操作
+            txManager.rollback();
+            throw new RuntimeException(e);
+        }finally{
+            //6.释放资源
+            txManager.release();
+        }
     }
 }
